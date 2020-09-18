2d60e517f1a8246ee1082bcb34931ea77d3c5519
ap3340 <59696451+ap3340@users.noreply.github.com>
2020-09-18 18:06:23 -0400

    Update Spot.java

--- src/a8/Spot.java
+++ src/a8/Spot.java
@@ -1,45 +1,59 @@
+import java.awt.Color;
 
-	import java.awt.Color;
+public interface Spot {
 
-	public interface Spot {
+	int getSpotX();
 
-		int getSpotX();
-		int getSpotY();
-		SpotBoard getBoard();
+	int getSpotY();
 
-		boolean isEmpty();
-		void setSpot();
-		void clearSpot();
-		default void toggleSpot() {
-			if (isEmpty()) {
-				setSpot();
-			} else {
-				clearSpot();
-			}
-		}
+	SpotBoard getBoard();
 
-		boolean isHighlighted();
-		void highlightSpot();
-		void unhighlightSpot();
-		default void toggleHighlight() {
-			if (isHighlighted()) {
-				unhighlightSpot();
-			} else {
-				highlightSpot();
-			}
-		}
+	boolean isEmpty();
 
-		void setBackground(Color c);
-		Color getBackground();
-		void setSpotColor(Color c);
-		Color getSpotColor();
-		void setHighlight(Color c);
-		Color getHighlight();
+	void setSpot();
 
-		void addSpotListener(SpotListener l);
-		void removeSpotListener(SpotListener l);
+	void clearSpot();
 
-		default String getCoordString() {
-			return "(" + getSpotX() + ", " + getSpotY() + ")";
+	default void toggleSpot() {
+		if (isEmpty()) {
+			setSpot();
+		} else {
+			clearSpot();
 		}
 	}
+
+	boolean isHighlighted();
+
+	void highlightSpot();
+
+	void unhighlightSpot();
+
+	default void toggleHighlight() {
+		if (isHighlighted()) {
+			unhighlightSpot();
+		} else {
+			highlightSpot();
+		}
+	}
+
+	void setBackground(Color c);
+
+	Color getBackground();
+
+	void setSpotColor(Color c);
+
+	Color getSpotColor();
+
+	void setHighlight(Color c);
+
+	Color getHighlight();
+
+	void addSpotListener(SpotListener l);
+
+	void removeSpotListener(SpotListener l);
+
+	default String getCoordString() {
+		return "(" + getSpotX() + ", " + getSpotY() + ")";
+	}
+}
+
